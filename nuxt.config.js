032321268b7500dc04342630590d9976cb3f3911
Billybobbonnet <boitalettre9@gmail.com>
2021-04-08 15:32:29 +0200

    updated router base to test on gh pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
+  router: {
+    base: '/Intercontinental-Academia/',
+  },
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
