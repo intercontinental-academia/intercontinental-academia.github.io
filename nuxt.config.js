4f9399016cba19ce2747fb51290a8e499d115e8b
Billybobbonnet <boitalettre9@gmail.com>
2021-05-05 09:45:05 +0200

    fix: attempt to fix primary color

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   target: 'static',
   generate: {
     fallback: true,
-    async ready () {
+    async ready() {
       const { $content } = require('@nuxt/content')
       const programs = await $content('Programs')
         .sortBy('_', 'desc')
