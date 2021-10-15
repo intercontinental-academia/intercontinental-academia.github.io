888406ef5193ba5dd781cc8515da6dd07abe61ca
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-10-15 15:23:54 +0000

    deploy: db911c1a000e8869bc7ae0a4f33a648836bec5cd

--- _nuxt/static/1634311155/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout\u002Fconcept","\u002Fmentors","\u002Fprogram","\u002Fblog","\u002Fabout\u002Fica4","\u002Ffellows","\u002Fmedia","\u002Fblog\u002FAlex Cayco Gajic","\u002Fblog\u002FDeshen Moodley","\u002Fprogram\u002Fsession-1","\u002Fprogram\u002Fopening-session","\u002Fprogram\u002Fsession-2"]})
