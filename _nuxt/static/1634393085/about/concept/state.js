21f22896326f949615e3791e6ad2a7499e8dad39
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-10-16 14:05:32 +0000

    deploy: b694bc82f6bfeaf63fbcba701900ab718ca89f37

--- _nuxt/static/1634393085/about/concept/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634393085",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout\u002Fconcept",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3097a3d8"}}}}(null));
