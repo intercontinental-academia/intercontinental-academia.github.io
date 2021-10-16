073b4ad422828b1dc1e4f2ae88ca01a0bc08ecc5
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-10-16 17:10:18 +0000

    deploy: 046b32a1fecab3931485be922b368beb43599319

---
+++ _nuxt/static/1634404195/blog/Laura Candiotto/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634404195",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002FLaura%20Candiotto",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"80aec790"}}}}(null));
