ffcfe9ea91e9bcf0a252c197c0e937f91f0c78a4
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-10-18 08:58:05 +0000

    deploy: f0bb89d79370b0e8605dd25d43ae5377ac6dfd40

---
+++ _nuxt/static/1634547460/blog/Alex Cayco Gajic/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634547460",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002FAlex%20Cayco%20Gajic",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b07a8b22"}}}}(null));
