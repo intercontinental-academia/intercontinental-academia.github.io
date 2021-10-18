17f5c1fa60ed5f4ba073c19aa7be682819ca9aa7
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-10-18 04:38:44 +0000

    deploy: cb23970125a215d72e05c390916d5493fdd61f31

--- _nuxt/static/1634531882/fellows/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634531882",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffellows",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b2ad8c6a"}}}}(null));
