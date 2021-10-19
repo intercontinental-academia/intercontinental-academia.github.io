8ea998d2dc8a63805aa0e3f88c9cf86834176b46
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-10-19 15:00:15 +0000

    deploy: b7d0f26dff73c2e542d491ce442f6bd797ab020e

--- _nuxt/static/1634655508/program/session-2/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634655508",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprogram\u002Fsession-2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3b0e35b9"}}}}(null));
