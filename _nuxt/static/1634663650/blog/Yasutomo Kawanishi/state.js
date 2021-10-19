27522f34d017a9ca686dfa128864db2eb7c6d433
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-10-19 17:14:41 +0000

    deploy: 2392a757a5679d2601c46f5e24b336ba92fdfcb0

---
+++ _nuxt/static/1634663650/blog/Yasutomo Kawanishi/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634663650",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002FYasutomo%20Kawanishi",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0768734d"}}}}(null));
