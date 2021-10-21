f7f31f10877edbedd4fcde1c5b6a0668236d9690
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-10-21 11:10:41 +0000

    deploy: e574fc6ce46499b467ee99c1c1cf7123caad60ad

---
+++ _nuxt/static/1634814587/blog/Tahina Ralitera/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634814587",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002FTahina%20Ralitera",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"aac5470c"}}}}(null));
