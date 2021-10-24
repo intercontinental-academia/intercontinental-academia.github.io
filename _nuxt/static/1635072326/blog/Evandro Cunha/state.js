d46e76de0f9db22fd1ced323e8c5b673a31e7fc6
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-10-24 10:46:31 +0000

    deploy: 6bd9980249058b2095c797ce0d9b0a46cfe2d449

---
+++ _nuxt/static/1635072326/blog/Evandro Cunha/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635072326",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002FEvandro%20Cunha",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1beeb412"}}}}(null));
