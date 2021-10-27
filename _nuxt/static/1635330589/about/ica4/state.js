b248e27d44743bbf82bda1cb919197b503987a56
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-10-27 10:32:00 +0000

    deploy: bfe77995d17a34547e671b4e91aa34c431253332

--- _nuxt/static/1635330589/about/ica4/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635330589",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout\u002Fica4",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"71b60094"}}}}(null));
