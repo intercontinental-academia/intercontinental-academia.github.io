df7afb257e7a562bdaaaa94e26e06e9f35ef07d1
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-11-03 17:22:58 +0000

    deploy: 11cf8b3af4139843e2ad22280139d41cd30269b4

--- _nuxt/static/1635960047/mentors/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635960047",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmentors",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"60481cc6"}}}}(null));
