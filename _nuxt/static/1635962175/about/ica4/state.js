a1419fa4e1641e64797a8a452f45b85ddca0f357
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-11-03 17:59:10 +0000

    deploy: 7bc6c34a835b6d437249bec1bbcb30b43a450497

--- _nuxt/static/1635962175/about/ica4/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635962175",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout\u002Fica4",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"481d34b3"}}}}(null));
--- _nuxt/static/1635962175/blog/Jakub Growiec/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635962175",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002FJakub%20Growiec",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"481d34b3"}}}}(null));
